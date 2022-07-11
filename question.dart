3f7173181da426d9af46dc4e2d9f8f9324829cee
ishantJ7 <88197494+ishantJ7@users.noreply.github.com>
2022-07-11 21:38:56 +0530

    Add files via upload

---
+++ question.dart
@@ -0,0 +1,10 @@
+class Question {
+  String questiontext;
+  bool questionanswer;
+
+  //  creating a constructor
+  Question({String q, bool a}){
+    questiontext = q;
+    questionanswer = a;
+  }
+}
